0d6019d8a2d7c7448039cf8e7fa0b7e3f5a793ba
Tito <github@mynameistito.com>
2026-04-19 15:21:59 +1200

    feat(ts): migrate remapper to TypeScript
    Typed from/to platform params and KeyDefines. Cast removed from
    keycodes.ts now that remapper exports a proper return type.

--- src/utils/remapper.ts
+++ src/utils/remapper.ts
@@ -1,46 +1,43 @@
-function remapper(from = 'standard', to = 'darwin', defines = {}) {
-  if (from == 'standard') {
-    // from standard linux, darwin, win32
+import type { Platform, KeyDefines } from '../libs/keycodes';
+
+function remapper(
+  from: 'standard' | Platform,
+  to: Platform,
+  defines: KeyDefines,
+): KeyDefines {
+  if (from === 'standard') {
     switch (to) {
       case 'linux':
         break;
-      case 'darwin': {
-        defines['91'] = defines['3639']; // mac f13
-        defines['92'] = defines['70']; // mac f14
-        defines['93'] = defines['3653']; // mac f15
+      case 'darwin':
+        defines['91'] = defines['3639'];
+        defines['92'] = defines['70'];
+        defines['93'] = defines['3653'];
         defines['91'] = defines['57416'];
         defines['56'] = defines['3675'];
         defines['3675'] = defines['56'];
         defines['3675'] = defines['3640'];
         defines['56'] = defines['3676'];
         defines['29'] = defines['3613'];
-
-        defines['3597'] = defines['69']; // numlock -> clear
+        defines['3597'] = defines['69'];
         break;
-      }
-      case 'win32': {
-        // row 2
+      case 'win32':
         defines['61010'] = defines['3666'];
         defines['60999'] = defines['3655'];
         defines['61001'] = defines['3657'];
-        // row 3
         defines['61011'] = defines['3667'];
         defines['61007'] = defines['3663'];
         defines['61009'] = defines['3665'];
         defines['3677'] = defines['3613'];
-        // row 4
         defines['61000'] = defines['57416'];
-        // row 5
         defines['61003'] = defines['57419'];
         defines['61008'] = defines['57424'];
         defines['61005'] = defines['57421'];
         break;
-      }
     }
   } else {
-    // from linux, darwin, win32 to standard
     switch (from) {
-      case 'darwin': {
+      case 'darwin':
         defines['3639'] = defines['91'];
         defines['70'] = defines['92'];
         defines['3653'] = defines['93'];
@@ -50,27 +47,23 @@ function remapper(from = 'standard', to = 'darwin', defines = {}) {
         defines['3676'] = defines['56'];
         defines['3613'] = defines['29'];
         break;
-      }
-      case 'win32': {
-        // row 2
+      case 'win32':
         defines['3666'] = defines['61010'];
         defines['3655'] = defines['60999'];
         defines['3657'] = defines['61001'];
-        // row 3
         defines['3667'] = defines['61011'];
         defines['3663'] = defines['61007'];
         defines['3665'] = defines['61009'];
-        // row 4
         defines['57416'] = defines['61000'];
-        // row 5
         defines['57419'] = defines['61003'];
         defines['57424'] = defines['61008'];
         defines['57421'] = defines['61005'];
         break;
-      }
+      case 'linux':
+        break;
     }
   }
   return defines;
 }
 
-module.exports = remapper;
+export default remapper;
